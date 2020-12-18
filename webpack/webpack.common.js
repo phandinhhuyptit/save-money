36d9c1281947102ba64d1dc2dd1ab3ff040edbd9
Phan Đình Huy <kompadev@KompaDevs-MacBook-Pro.local>
2020-12-18 17:39:51 +0700

    [UPDATE] config react

---
+++ webpack/webpack.common.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, '..', './src/index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              // Inline files smaller than 10 kB
+              limit: 10 * 1024
+            }
+          }
+        ]
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'svg-url-loader',
+            options: {
+              // Inline files smaller than 10 kB
+              limit: 10 * 1024
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Huy IT',
+      template: path.resolve(__dirname, '..', './src/index.html')
+    })
+  ],
+  output: {
+    path: path.join(__dirname, '..', 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  }
+};
---
+++ webpack/webpack.prod.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const webpack = require("webpack");
+const Dotenv = require("dotenv-webpack");
+
+module.exports = {
+  mode: "development",
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.ProvidePlugin({
+      ReactDOM: "react-dom",
+      React: "react",
+    }),
+    new Dotenv({
+      path: path.resolve(__dirname, "..", "./.env.development"),
+    }),
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, "..", "./dist"),
+    historyApiFallback: true,
+    hot: true,
+  },
+  devtool: "eval-source-map",
+};
